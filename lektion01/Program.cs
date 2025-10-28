edf3309e2a5a102bfa8587c340a2f8ab69a6ead8
Krister Trangius <krister@trangius.se>
2025-10-28 13:22:22 +0100

    add lektion01 example

---
+++ lektion01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Data;
+using MySql.Data.MySqlClient;
+using Dapper;
+
+class Teacher
+{
+    public string Name { set; get; }
+    public string Email { set; get; }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // skapa en connectionstring mot min databas:
+        string constring = File.ReadLines("connectionstring.txt").First();
+
+        // skapa ett IDBConnection-objekt (dbcon) av MySQL-typ:
+        using IDbConnection dbcon = new MySqlConnection(constring);
+
+        // lägg in en lärare:
+        //dbcon.Execute("INSERT INTO Teacher (Name, Email) VALUES ('Janne Långben', 'janne@suvnet.se');");
+
+        // skapa en lista med teachers, fyll på den med data från ett result set
+        // genom att anropa dapper-metoden Query():
+        IEnumerable<Teacher> teachers = dbcon.Query<Teacher>("select Name, Email from Teacher;");
+
+        // loopa igenom teachers-ienumerablen (typ lista) och skriv ut:
+        foreach (Teacher t in teachers)
+        {
+            Console.WriteLine($"Hello from {t.Name}, you can reach me at {t.Email}");
+        }
+    }
+}
