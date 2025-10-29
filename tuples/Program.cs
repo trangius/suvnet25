957941ca4ab847b9e4d957b18941b7b64e91339b
Krister Trangius <krister@trangius.se>
2025-10-29 10:58:27 +0100

    add tuples (standalone and with Dapper) examples

---
+++ tuples/Program.cs
@@ -0,0 +1,7 @@
+﻿// Utan namngivna element
+var fia = ("Fia", 25, 173);
+Console.WriteLine($"{fia.Item1} är {fia.Item2} år gammal och är {fia.Item3} cm lång");
+
+// Med namngivna element
+var lisa = (Name:"Lisa", Age:30, Height:165);
+Console.WriteLine($"{lisa.Name} är {lisa.Age} år gammal och är {lisa.Height} cm lång");
